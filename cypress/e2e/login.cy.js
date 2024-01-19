f80fc8b11de687f1ac63ca74249472bb803c438d
Roma <153681980+RomaRed-qa@users.noreply.github.com>
2024-01-19 05:14:29 +0300

    Add files via upload

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,92 @@
+describe('Автотесты на форму логина', function () {
+    it('Верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        })
+
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#forgotForm > .header').should('be.visible');
+        cy.get('#forgotForm > .header').contains('Восстановите пароль');
+        cy.get('#exitRestoreButton > .exitIcon').should('be.visible');
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+       })
+
+
+      it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio2');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible')
+        })
+
+
+})
+       it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov22.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible')
+        })
+    
+
+        it('Логин без @ и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+         
+        
+        })
+
+
+        it('Логин со строчными буквами', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        })
+
+
+
+        describe('Покупка аватара', function () {
+        it('e2e тест на покупку нового аватара для тренера', function () {
+        cy.visit('https://pokemonbattle.me/');
+        cy.get(':nth-child(1) > .auth__input').type('roma.redchenko@mail.ru');
+        cy.get('#password').type('R3021400');
+        cy.get('.auth__button').click();
+        cy.get('.header__btns > [href="/shop"]').click();
+        cy.get('.shop__list > li').not('.feature-empty').children('.shop__button').eq(0).click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('Roman Redchenko');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+    })
+})
